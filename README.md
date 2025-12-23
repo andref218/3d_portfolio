c7164d1f6f2a16dcdfd67faa1de4bb8aa96f8eed
Andre Fonseca <andrefonseca.aff@gmail.com>
2025-12-23 16:19:52 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3D Portfolio
+
+Interactive 3D portfolio using Three.js
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <main>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1>Welcome to my 3D Portfolio!</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
